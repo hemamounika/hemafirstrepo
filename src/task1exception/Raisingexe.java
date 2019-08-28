d95ef78fab940b983c1d31ae710866d3492c20bd
Hema Mallabathula <hmallabathula@corp.miraclesoft.com>
2019-08-28 16:59:36 +0530

    firstcommit

---
+++ src/task1exception/Raisingexe.java
@@ -0,0 +1,47 @@
+package task1exception;
+
+import java.util.Scanner;
+
+public class Raisingexe
+{ 
+	
+	
+	
+	
+	
+    public static void validatewithdraw(int money)throws InvalidException
+    {  
+    int balance=500;
+    if(money > 500)  
+     throw new InvalidException("insuffficient money");
+    else 
+     System.out.println("valied money");  
+    }
+    
+    
+    
+    
+   public static void main(String args[])
+   {  
+	   Scanner sc=new Scanner(System.in);
+	   int x=sc.nextInt();  
+     try
+     {  
+     validatewithdraw(x);  
+     }
+     catch(Exception m)
+     {
+   	 System.out.println("Exception occured: "+m);
+   	 System.out.println("errorcode"+"    "+75875646);
+     }    
+ }  
+   
+class InvalidException extends Exception
+{  
+InvalidException(String s)
+{  
+ super(s);  
+}  
+
+}
+}
